e601e5ac719d80b7317308f98f8db7166082a126
Arthur Bicalho <arthurbic95@gmail.com>
2022-01-08 00:11:40 -0300

    Drawer Routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 import Home from "./pages/Home";
 import MyLinks from "./pages/MyLinks";
@@ -9,10 +10,46 @@ const Drawer = createDrawerNavigator();
 
 function Routes() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Home} />
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: false,
+        drawerActiveBackgroundColor: "#2CCBB9",
+        drawerActiveTintColor: "#FFF",
+        drawerStyle: {
+          fontSize: 19,
+          marginTop: 16,
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: "Encurtar Link",
+          drawerIcon: ({ focused, size, color }) => (
+            <Ionicons
+              name={focused ? "cube" : "cube-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
 
-      <Drawer.Screen name="MyLinks" component={MyLinks} />
+      <Drawer.Screen
+        name="MyLinks"
+        component={MyLinks}
+        options={{
+          title: "Meus Links",
+          drawerIcon: ({ focused, size, color }) => (
+            <Ionicons
+              name={focused ? "stats-chart" : "stats-chart-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
