de05acc684689bdec4fb38799dfadbd454b17d1a
Arthur Bicalho Lana Correa Fernandes <arthur.fernandes@syngenta.com>
2022-01-17 16:13:12 -0300

    EP 2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,78 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+
+import { LinearGradient } from "expo-linear-gradient";
+import {
+  ContainerLogo,
+  Logo,
+  ContainerContent,
+  Title,
+  Subtitle,
+  ContainerInput,
+  BoxIcon,
+  Input,
+  ButtonLink,
+  ButtonLinkText,
+} from "./styles";
+import { Feather } from "@expo/vector-icons";
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+
+import StatusBarPage from "../../components/StatusBarPage";
+import Menu from "../../components/Menu";
 
 export default function Home() {
+  const [input, setInput] = useState("");
+
   return (
-    <View>
-      <Text>Página Home</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <LinearGradient
+        colors={["#1DDBB9", "#132742"]}
+        style={{ flex: 1, justifyContent: "center" }}
+      >
+        <StatusBarPage barStyle="light-content" backgroundColor="#1DDBB9" />
+
+        <Menu />
+
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "android" ? "padding" : "position"}
+          enabled={true}
+        >
+          <ContainerLogo>
+            <Logo
+              source={require("../../assets/Logo.png")}
+              resizeMode="contain"
+            />
+          </ContainerLogo>
+
+          <ContainerContent>
+            <Title>SujeitoLink</Title>
+            <Subtitle>Cole seu link para encurtar</Subtitle>
+
+            <ContainerInput>
+              <BoxIcon>
+                <Feather name="link" size={22} color="#FFF" />
+              </BoxIcon>
+              <Input
+                placeholder="Cole seu link aqui..."
+                placeholderTextColor="white"
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardType="url"
+                value={input}
+                onChangeText={(text) => setInput(text)}
+              />
+            </ContainerInput>
+
+            <ButtonLink>
+              <ButtonLinkText>Gerar link</ButtonLinkText>
+            </ButtonLink>
+          </ContainerContent>
+        </KeyboardAvoidingView>
+      </LinearGradient>
+    </TouchableWithoutFeedback>
   );
 }
